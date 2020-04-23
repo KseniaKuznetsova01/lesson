40c2b5e251a3380e585eac83a8592da8b9bdf752
KatyaPanukova <55399352+KatyaPanukova@users.noreply.github.com>
2020-04-23 08:34:26 +0300

    Update Number.py

--- Number.py
+++ Number.py
@@ -1,29 +1,27 @@
 
 class Rim:
     def __init__(self, number):
-        d = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-        N = 0
-        PrevLetter = None
+        prev_letter = None
         key = True
-        ROMENUMBER = 'IVXLCDM'
-        CountRepeat = 0
-        CountLess = 0
+        rome_num = 'IVXLCDM'
+        count_rep = 0
+        count_les = 0
         for i in number:
             i = i.upper()
             if i not in 'IVXLCDM':
                 key = False
-            if PrevLetter and ROMENUMBER.index(PrevLetter) < ROMENUMBER.index(i):
-                CountLess += 1
-                CountRepeat = 0
-            elif PrevLetter and ROMENUMBER.index(PrevLetter) == ROMENUMBER.index(i):
-                CountRepeat += 1
-                CountLess = 0
+            if prev_letter and rome_num.index(prev_letter) < rome_num.index(i):
+                count_les += 1
+                count_rep = 0
+            elif prev_letter and rome_num.index(prev_letter) == rome_num.index(i):
+                count_rep += 1
+                count_les = 0
             else:
-                CountLess = 0
-                CountRepeat = 0
-            if CountLess > 1 or CountRepeat > 3:
+                count_les = 0
+                count_rep = 0
+            if count_les > 1 or count_rep > 3:
                 key = False
-            PrevLetter = i
+            prev_letter = i
 
             if key == True:
                 self.number = number
@@ -33,6 +31,7 @@ class Rim:
         self.number = number
 
     def translate(self, number):
+        d = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
         s = 0
         for i in range(len(number)):
             if i > 0 and d[number[i]] > d[number[i - 1]]:
