755a25964d1ed313ac04958364ba04070f4fdbbf
KatyaPanukova <55399352+KatyaPanukova@users.noreply.github.com>
2020-04-24 15:23:42 +0300

    Update Rim.py

--- Rim.py
+++ Rim.py
@@ -1,4 +1,4 @@
-def check_input(number):
+def check_input(number):                                                   # Function for checking the entered number.
     key = True
     rome_num = 'IVXLCDM'
     new_num = ' ' + number
@@ -23,19 +23,20 @@ def check_input(number):
                 key = False
                 break
         else:
-            fix_str = rome_num[rome_num.find(i):]
+            fix_str = rome_num[rome_num.find(i):]                          # All values greater than the current value.
             if new_num[n] in fix_str:
                 key = False
                 break
             
         c += 1
         n += 1
+        
     if key:
         return number
     else:
         return 'None'
 
-def translate(number):
+def translate(number):                                                     # Function for converting a number from Roman to natural.
     d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     s = 0
     for i in range(len(number)):
@@ -46,15 +47,15 @@ def translate(number):
     return s
 
 
-class Rim:
-    def __init__(self, number):
-        self.number = check_input(number).upper()
+class Rim:                                                                  # Creating a class.
+    def __init__(self, number):                                             # Creating an instance of the class.
+        self.number = check_input(number).upper()                           # Creating an object attribute from a class.
 
-    def __str__(self):
+    def __str__(self):                                                      # String representation method.
         if self.number == 'None':
             return 'None'
         else:
             return translate(self.number)
 
-    def __repr__(self):
+    def __repr__(self):                                                     # Getting an instance of the class.
         return self.__str__()
