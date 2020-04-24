c17744aea431b622ea10436b89d503f8bf844b67
KatyaPanukova <55399352+KatyaPanukova@users.noreply.github.com>
2020-04-24 14:27:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-from Rim import *
+from RomeNumerals import Rim
 
-number_1 = Rim('XII')
-print(number_1)
+print("I am a program that can work with Roman numbers.\n"
+      "If you want end this program, write: \" exit \" " "\n"
+      "Please enter your Roman number!")
+key = True
+
+while key:
+    number = input("\t")
+    if number == "exit":
+        key = False
+        break
+    num_rim = Rim(number)
+    try:
+      if num_rim.__str__() == 'None':
+          print("You entered an invalid number. Please, try again")
+          continue
+      print("\tYour number: ", number)
+      print("\tConvert number: ", num_rim.__str__())
+    except KeyError:
+        print("You entered an invalid number. Please, try again")
+        continue
