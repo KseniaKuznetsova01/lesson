ad7f979f94fd01371bdfe57fb1503c35f05f4e15
KseniaKuznetsova01 <ksenks.one@gmail.com>
2020-04-29 23:48:18 +0700

    3

--- main_ufo.py
+++ main_ufo.py
@@ -2,7 +2,7 @@ from Ufo import *
 import turtle as tr
 
 t = True
-
+tr.tracer(0)
 tr.hideturtle()
 ufo1 = Ufo('Пришелец-1', 100, 200, 150, 'green', 'black','yellow','blue', 5, 6)
 ufo2 = Ufo('Пришелец-2', 50, 50, 200, 'red','pink','blue','yellow', 3, 4)
