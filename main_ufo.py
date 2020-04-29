adc2c279a898257f3a2a54cfc1440b48dccbce79
KseniaKuznetsova01 <ksenks.one@gmail.com>
2020-04-29 15:17:36 +0700

    2

---
+++ main_ufo.py
@@ -0,0 +1,13 @@
+from Ufo import *
+import turtle as tr
+
+tr.tracer(0)
+tr.hideturtle()
+ufo1 = Ufo('Пришелец-1', 100, 200, 150, 'green', 'black','yellow','', 5, 6)
+ufo2 = Ufo('Пришелец-2', 50, 50, 200, 'red', 3, 4)
+while
+ufo1.show()
+ufo2.show()
+tr.done
+print(ufo1, '\n')
+print(ufo2.engine_grade)
