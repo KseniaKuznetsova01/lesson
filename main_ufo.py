c999f9f8292da0816b4d6b29129e11c4caff6d6d
KseniaKuznetsova01 <ksenks.one@gmail.com>
2020-04-29 17:05:57 +0700

    2

--- main_ufo.py
+++ main_ufo.py
@@ -5,7 +5,7 @@ tr.tracer(0)
 tr.hideturtle()
 ufo1 = Ufo('Пришелец-1', 100, 200, 150, 'green', 'black','yellow','', 5, 6)
 ufo2 = Ufo('Пришелец-2', 50, 50, 200, 'red', 3, 4)
-while
+
 ufo1.show()
 ufo2.show()
 tr.done
