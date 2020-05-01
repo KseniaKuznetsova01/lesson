0a5b0120539e68b2e033a6288d33df5a90b2d2e3
KatyaPanukova <55399352+KatyaPanukova@users.noreply.github.com>
2020-05-01 13:24:02 +0300

    Update Dima_main.py

--- Dima_main.py
+++ Dima_main.py
@@ -2,16 +2,17 @@ from ufo import *
 import turtle as tr
 import time
 
+start_time = time.time()
 timer = int(input('Введите время работы программы: '))
 tr.tracer(0)
 tr.hideturtle()
-ufo1 = Ufo('Пришелец-1', 100, 200, 150, 'green', 'black', 'yellow', 'blue', 5, 6, 5)
-ufo2 = Ufo('Пришелец-2', 50, 50, 200, 'red', 'pink', 'blue', 'yellow', 3, 4, 10)
+ufo1 = Ufo('Пришелец-1', 50, 50, 50, 'green', 'black', 'yellow', 'blue', 5, 6, 5)
+ufo2 = Ufo('Пришелец-2', -50, 50, 20, 'red', 'pink', 'blue', 'yellow', 3, 4, 10)
 
 ufo1.show()
 ufo2.show()
 
-while time.sleep(timer) != None:
+while (time.time() - start_time) < timer:
     tr.clear()
     ufo1.move()
     ufo2.move()
@@ -20,3 +21,4 @@ tr.done()
 
 print(ufo1, '\n')
 print(ufo2.engine_grade)
+
