fc6b60187050df4202614c2333dedd55ae7b484c
KatyaPanukova <55399352+KatyaPanukova@users.noreply.github.com>
2020-05-01 11:48:06 +0300

    Update Dima_main.py

--- Dima_main.py
+++ Dima_main.py
@@ -1,18 +1,18 @@
 from ufo import *
 import turtle as tr
-
+import time
 
 tr.tracer(0)
 tr.hideturtle()
 ufo1 = Ufo('Пришелец-1', 100, 200, 150, 'green', 'black', 'yellow', 'blue', 5, 6, 5)
 ufo2 = Ufo('Пришелец-2', 50, 50, 200, 'red', 'pink', 'blue', 'yellow', 3, 4, 10)
 
-time = 1000
+timer = int(input('Введите время работы программы: '))
 
 ufo1.show()
 ufo2.show()
 
-for i in range(time):
+while time.sleep(timer) != None:
     tr.clear()
     ufo1.move()
     ufo2.move()
